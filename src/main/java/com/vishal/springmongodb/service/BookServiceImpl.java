c487ad1e91ef96294949164afd04deef4f76f009
Vishal <apple@APPLEs-MacBook-Air.local>
2023-09-26 02:03:02 +0530

    aggregation changes

--- src/main/java/com/vishal/springmongodb/service/BookServiceImpl.java
+++ src/main/java/com/vishal/springmongodb/service/BookServiceImpl.java
@@ -5,26 +5,57 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.GroupOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import com.vishal.springmongodb.controller.BookController;
 import com.vishal.springmongodb.modal.Book;
 import com.vishal.springmongodb.modal.BookCountByAuthorName;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
 public class BookServiceImpl {
 
 	@Autowired
     private MongoTemplate mongoTemplate;
 
 	 public List<Book> searchBooks(String keyword, String author) {
-	        Query query = new Query();
-	        query.addCriteria(Criteria.where("bookName").regex(keyword, "i"))
-	        		.addCriteria(Criteria.where("authorName").is(author));
-	        return mongoTemplate.find(query, Book.class);
+//	        Query query = new Query();
+//	        query.addCriteria(Criteria.where("bookName").regex(keyword, "i"))
+//	        		.addCriteria(Criteria.where("authorName").is(author));
+	        
+	        AggregationOperation match = Aggregation.match(
+	                new Criteria().orOperator(
+	                    new Criteria().andOperator(
+	                        Criteria.where("bookName").is("Two States"),
+	                        Criteria.where("authorName").is("chetha")
+	                        // Add more AND conditions here as needed
+	                    ),
+	                    new Criteria().andOperator(
+	                    		Criteria.where("bookName").is("Love"),
+		                        Criteria.where("authorName").is("vishal")
+	                        // Add more AND conditions here as needed
+	                    ),
+	                    new Criteria().andOperator(
+	                    		Criteria.where("bookName").is("gayathri"),
+		                        Criteria.where("authorName").is("vishal")
+	                        // Add more AND conditions here as needed
+	                    )
+	                )
+	            );
+	        
+	        Aggregation aggregation = Aggregation.newAggregation(match);
+	        List<Book> books = mongoTemplate.aggregate(aggregation, "Book", Book.class).getMappedResults();
+	        log.info("books: {} ", books);
+	        return books;
+	        
+//	        return mongoTemplate.find(query, Book.class);
 	    }
 
 	    // Example of using getDocumentBasedQuery for custom queries
